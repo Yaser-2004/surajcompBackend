39240ffdb0940b506421cb20a4aa908237eb4ee7
Yaser Siddiquee <yaser.siddiquee@gmail.com>
2026-01-07 20:52:47 +0530

    new commit

--- controllers/enquiryController.js
+++ controllers/enquiryController.js
@@ -1,29 +1,36 @@
 import sendEmail from "../config/mailer.js";
 
 export const sendEnquiry = async (req, res) => {
-  const { name, mobile, email, service, message } = req.body;
+  try {
+    const { name, mobile, email, service, message } = req.body;
 
-  if (!name || !mobile || !message) {
-    return res.status(400).json({ message: "Missing required fields" });
+    if (!name || !mobile || !message) {
+      return res.status(400).json({ message: "Missing required fields" });
+    }
+
+    const emailContent = `
+      New Enquiry Received
+
+      Name: ${name}
+      Mobile: ${mobile}
+      Email: ${email || "N/A"}
+      Service: ${service || "N/A"}
+
+      Message:
+      ${message}
+          `;
+
+    await sendEmail(
+      process.env.EMAIL_USER,
+      "New Website Enquiry - Suraj Computer",
+      emailContent
+    );
+
+    res.json({ message: "Enquiry sent successfully" });
+
+  } catch (error) {
+    console.error("Enquiry Error:", error);
+    res.status(500).json({ message: "Failed to send enquiry" });
   }
-
-  const emailContent = `
-    New Enquiry Received
-
-    Name: ${name}
-    Mobile: ${mobile}
-    Email: ${email || "N/A"}
-    Service: ${service || "N/A"}
-
-    Message:
-    ${message}
-  `;
-
-  await sendEmail(
-    process.env.EMAIL_USER,
-    "New Website Enquiry - Suraj Computer",
-    emailContent
-  );
-
-  res.json({ message: "Enquiry sent successfully" });
 };
+
